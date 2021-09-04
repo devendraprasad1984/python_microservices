ab1ff914d7a9a7ab1abc0d1d513e6b202860a961
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-04 13:29:04 +0530

    updates

---
+++ user/models.py
@@ -0,0 +1,35 @@
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+# connect this app to this db tied to this app
+def init_app(app):
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    is_admin = db.Column(db.Boolean)
+    api_key = db.Column(db.String(255), unique=True, nullable=True)
+    is_active = db.Column(db.Boolean, default=True)
+    authenticated = db.Column(db.Boolean, default=False)
+
+
+    def __repr__(self):
+        return f'user {self.id}, {self.username}'
+
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'is_admin': self.is_admin,
+            'api_key': self.api_key,
+            'is_active': self.is_active,
+            'authenticated': self.authenticated,
+        }
